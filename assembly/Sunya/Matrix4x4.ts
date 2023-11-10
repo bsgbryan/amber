d8019f1dc68b0d4867d06f75d976021bfcbd09a6
Bryan Maynard <bsgbryan@gmail.com>
2023-11-10 15:00:51 -0600

    Implemented SIMD for all Matrix4x4 functions that needed it!

--- assembly/Sunya/Matrix4x4.ts
+++ assembly/Sunya/Matrix4x4.ts
@@ -11,16 +11,16 @@ export function aim(
 ): Float32Array {
   const output = new Float32Array(16)
 
-  const zAxis = normalize(subtract(target, eye  ))
-  const xAxis = normalize(cross   (up,     zAxis))
-  const yAxis = normalize(cross   (zAxis,  xAxis))
+  const z_axis = normalize(subtract(target, eye   ))
+  const x_axis = normalize(cross   (up,     z_axis))
+  const y_axis = normalize(cross   (z_axis, x_axis))
 
   // The first three items here are negated so the X axis isn't inverted
   // (I have no idea why this happens, but it's super confusing and disorienting)
-  output[ 0] = -xAxis[0]; output[ 1] = -xAxis[1]; output[ 2] = -xAxis[2]; output[ 3] = 0;
-  output[ 4] =  yAxis[0]; output[ 5] =  yAxis[1]; output[ 6] =  yAxis[2]; output[ 7] = 0;
-  output[ 8] =  zAxis[0]; output[ 9] =  zAxis[1]; output[10] =  zAxis[2]; output[11] = 0;
-  output[12] =    eye[0]; output[13] =    eye[1]; output[14] =    eye[2]; output[15] = 1;
+  output[ 0] = -x_axis[0]; output[ 1] = -x_axis[1]; output[ 2] = -x_axis[2]; output[ 3] = 0;
+  output[ 4] =  y_axis[0]; output[ 5] =  y_axis[1]; output[ 6] =  y_axis[2]; output[ 7] = 0;
+  output[ 8] =  z_axis[0]; output[ 9] =  z_axis[1]; output[10] =  z_axis[2]; output[11] = 0;
+  output[12] =     eye[0]; output[13] =     eye[1]; output[14] =     eye[2]; output[15] = 1;
 
   return output
 }
@@ -32,20 +32,28 @@ export function look_at(
 ): Float32Array {
   const output = new Float32Array(16)
 
-  const zAxis = normalize(subtract(eye,   target)),
-        xAxis = normalize(cross   (up,    zAxis )),
-        yAxis = normalize(cross   (zAxis, xAxis ))
+  const z_axis = normalize(subtract(eye,    target)),
+        x_axis = normalize(cross   (up,     z_axis)),
+        y_axis = normalize(cross   (z_axis, x_axis))
 
-  const x_eye = -(xAxis[0] * eye[0] + xAxis[1] * eye[1] + xAxis[2] * eye[2]),
-        y_eye = -(yAxis[0] * eye[0] + yAxis[1] * eye[1] + yAxis[2] * eye[2]),
-        z_eye = -(zAxis[0] * eye[0] + zAxis[1] * eye[1] + zAxis[2] * eye[2])
+  const v0 = f32x4(x_axis[0], x_axis[1], x_axis[2], 0),
+        v1 = f32x4(y_axis[0], y_axis[1], y_axis[2], 0),
+        v2 = f32x4(z_axis[0], z_axis[1], z_axis[2], 0),
+        v3 = f32x4(   eye[0],    eye[1],    eye[2], 0),
+        r0 = f32x4.mul(v0, v3),
+        r1 = f32x4.mul(v1, v3),
+        r2 = f32x4.mul(v2, v3)
+
+  const x_eye = -(f32x4.extract_lane(r0, 0) + f32x4.extract_lane(r0, 1) + f32x4.extract_lane(r0, 2)),
+        y_eye = -(f32x4.extract_lane(r1, 0) + f32x4.extract_lane(r1, 1) + f32x4.extract_lane(r1, 2)),
+        z_eye = -(f32x4.extract_lane(r2, 0) + f32x4.extract_lane(r2, 1) + f32x4.extract_lane(r2, 2))
 
   // The first three items here are negated so the X axis isn't inverted
   // (I have no idea why this happens, but it's super confusing and disorienting)
-  output[ 0] = -xAxis[0]; output[ 1] = -yAxis[0]; output[ 2] = -zAxis[0]; output[ 3] = 0;
-  output[ 4] =  xAxis[1]; output[ 5] =  yAxis[1]; output[ 6] =  zAxis[1]; output[ 7] = 0;
-  output[ 8] =  xAxis[2]; output[ 9] =  yAxis[2]; output[10] =  zAxis[2]; output[11] = 0;
-  output[12] =  x_eye;    output[13] =  y_eye;    output[14] =  z_eye;    output[15] = 1;
+  output[ 0] = -x_axis[0]; output[ 1] = -y_axis[0]; output[ 2] = -z_axis[0]; output[ 3] = 0;
+  output[ 4] =  x_axis[1]; output[ 5] =  y_axis[1]; output[ 6] =  z_axis[1]; output[ 7] = 0;
+  output[ 8] =  x_axis[2]; output[ 9] =  y_axis[2]; output[10] =  z_axis[2]; output[11] = 0;
+  output[12] =  x_eye;     output[13] =  y_eye;     output[14] =  z_eye;     output[15] = 1;
 
   return output
 }
@@ -56,58 +64,58 @@ export function multiply(
 ): Float32Array {
   const output = new Float32Array(16)
 
-  const b00 = b[0 * 4 + 0]
-  const b01 = b[0 * 4 + 1]
-  const b02 = b[0 * 4 + 2]
-  const b03 = b[0 * 4 + 3]
-  const b10 = b[1 * 4 + 0]
-  const b11 = b[1 * 4 + 1]
-  const b12 = b[1 * 4 + 2]
-  const b13 = b[1 * 4 + 3]
-  const b20 = b[2 * 4 + 0]
-  const b21 = b[2 * 4 + 1]
-  const b22 = b[2 * 4 + 2]
-  const b23 = b[2 * 4 + 3]
-  const b30 = b[3 * 4 + 0]
-  const b31 = b[3 * 4 + 1]
-  const b32 = b[3 * 4 + 2]
-  const b33 = b[3 * 4 + 3]
-  const a00 = a[0 * 4 + 0]
-  const a01 = a[0 * 4 + 1]
-  const a02 = a[0 * 4 + 2]
-  const a03 = a[0 * 4 + 3]
-  const a10 = a[1 * 4 + 0]
-  const a11 = a[1 * 4 + 1]
-  const a12 = a[1 * 4 + 2]
-  const a13 = a[1 * 4 + 3]
-  const a20 = a[2 * 4 + 0]
-  const a21 = a[2 * 4 + 1]
-  const a22 = a[2 * 4 + 2]
-  const a23 = a[2 * 4 + 3]
-  const a30 = a[3 * 4 + 0]
-  const a31 = a[3 * 4 + 1]
-  const a32 = a[3 * 4 + 2]
-  const a33 = a[3 * 4 + 3]
+  const a0 = f32x4(a[0], a[4], a[8],  a[12]),
+        a1 = f32x4(a[1], a[5], a[9],  a[13]),
+        a2 = f32x4(a[2], a[6], a[10], a[14]),
+        a3 = f32x4(a[3], a[7], a[11], a[15])
 
-  output[ 0] = b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30
-  output[ 1] = b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31
-  output[ 2] = b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32
-  output[ 3] = b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33
+  const b0 = f32x4(b[0], b[1], b[2], b[3])
 
-  output[ 4] = b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30
-  output[ 5] = b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31
-  output[ 6] = b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32
-  output[ 7] = b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33
+  const r0 = f32x4.mul(a0, b0),
+        r1 = f32x4.mul(a1, b0),
+        r2 = f32x4.mul(a2, b0),
+        r3 = f32x4.mul(a3, b0)
 
-  output[ 8] = b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30
-  output[ 9] = b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31
-  output[10] = b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32
-  output[11] = b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33
+  output[0] = f32x4.extract_lane(r0, 0) + f32x4.extract_lane(r0, 1) + f32x4.extract_lane(r0, 2) + f32x4.extract_lane(r0, 3)
+  output[1] = f32x4.extract_lane(r1, 0) + f32x4.extract_lane(r1, 1) + f32x4.extract_lane(r1, 2) + f32x4.extract_lane(r1, 3)
+  output[2] = f32x4.extract_lane(r2, 0) + f32x4.extract_lane(r2, 1) + f32x4.extract_lane(r2, 2) + f32x4.extract_lane(r2, 3)
+  output[3] = f32x4.extract_lane(r3, 0) + f32x4.extract_lane(r3, 1) + f32x4.extract_lane(r3, 2) + f32x4.extract_lane(r3, 3)
 
-  output[12] = b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30
-  output[13] = b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31
-  output[14] = b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32
-  output[15] = b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33
+  const b1 = f32x4(b[4], b[5], b[6], b[7])
+
+  const r4 = f32x4.mul(a0, b1),
+        r5 = f32x4.mul(a1, b1),
+        r6 = f32x4.mul(a2, b1),
+        r7 = f32x4.mul(a3, b1)
+
+  output[4] = f32x4.extract_lane(r4, 0) + f32x4.extract_lane(r4, 1) + f32x4.extract_lane(r4, 2) + f32x4.extract_lane(r4, 3)
+  output[5] = f32x4.extract_lane(r5, 0) + f32x4.extract_lane(r5, 1) + f32x4.extract_lane(r5, 2) + f32x4.extract_lane(r5, 3)
+  output[6] = f32x4.extract_lane(r6, 0) + f32x4.extract_lane(r6, 1) + f32x4.extract_lane(r6, 2) + f32x4.extract_lane(r6, 3)
+  output[7] = f32x4.extract_lane(r7, 0) + f32x4.extract_lane(r7, 1) + f32x4.extract_lane(r7, 2) + f32x4.extract_lane(r7, 3)
+
+  const b2 = f32x4(b[8], b[9], b[10], b[11])
+
+  const r8  = f32x4.mul(a0, b2),
+        r9  = f32x4.mul(a1, b2),
+        r10 = f32x4.mul(a2, b2),
+        r11 = f32x4.mul(a3, b2)
+
+  output[ 8] = f32x4.extract_lane(r8,  0) + f32x4.extract_lane(r8,  1) + f32x4.extract_lane(r8,  2) + f32x4.extract_lane(r8,  3)
+  output[ 9] = f32x4.extract_lane(r9,  0) + f32x4.extract_lane(r9,  1) + f32x4.extract_lane(r9,  2) + f32x4.extract_lane(r9,  3)
+  output[10] = f32x4.extract_lane(r10, 0) + f32x4.extract_lane(r10, 1) + f32x4.extract_lane(r10, 2) + f32x4.extract_lane(r10, 3)
+  output[11] = f32x4.extract_lane(r11, 0) + f32x4.extract_lane(r11, 1) + f32x4.extract_lane(r11, 2) + f32x4.extract_lane(r11, 3)
+
+  const b3 = f32x4(b[12], b[13], b[14], b[15])
+
+  const r12 = f32x4.mul(a0, b3),
+        r13 = f32x4.mul(a1, b3),
+        r14 = f32x4.mul(a2, b3),
+        r15 = f32x4.mul(a3, b3)
+
+  output[12] = f32x4.extract_lane(r12, 0) + f32x4.extract_lane(r12, 1) + f32x4.extract_lane(r12, 2) + f32x4.extract_lane(r12, 3)
+  output[13] = f32x4.extract_lane(r13, 0) + f32x4.extract_lane(r13, 1) + f32x4.extract_lane(r13, 2) + f32x4.extract_lane(r13, 3)
+  output[14] = f32x4.extract_lane(r14, 0) + f32x4.extract_lane(r14, 1) + f32x4.extract_lane(r14, 2) + f32x4.extract_lane(r14, 3)
+  output[15] = f32x4.extract_lane(r15, 0) + f32x4.extract_lane(r15, 1) + f32x4.extract_lane(r15, 2) + f32x4.extract_lane(r15, 3)
 
   return output
 }
