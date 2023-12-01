873593ea035ad8ae629929d673f43a5b64626891
Bryan Maynard <bsgbryan@gmail.com>
2023-12-01 12:05:39 -0600

    Removed more unneeded type definitions

--- src/Benzaiten/types.ts
+++ src/Benzaiten/types.ts
@@ -1,14 +1,5 @@
 export type Vector3 = Float32Array
 
-export type TestResult = Vector3 | null | undefined
-
-export type Output = {
-  debug:   Array<number>
-  x_cross: Array<number>
-  y_cross: Array<number>
-  z_cross: Array<number>
-}
-
 export type Mesh = {
   vertices: Float32Array
   indices?: Uint16Array
