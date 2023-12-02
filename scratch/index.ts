6837a6cbf31b7d2ddbdffea262336479ec5654a1
Bryan Maynard <bsgbryan@gmail.com>
2023-12-02 16:55:57 -0600

    Pretty major refactor of Benzaiten
    In preparation for the migration to WebAssembly, the class is now gone; Benzaiten is now a signle function that takes all the data it needs as params - no more `this`!

--- scratch/index.ts
+++ scratch/index.ts
@@ -1,20 +1,24 @@
-import { Sphere } from "#/Benzaiten/shapes"
+import MainCamera                         from "@/Athenaeum/components/MainCamera"
+import SimpleVertexColor                  from "@/Athenaeum/materials/SimpleVertexColor"
+import Update_Camera_and_Target_Positions from "@/Athenaeum/systems/Update_Camera_and_Target_Positions"
+
+import { Sphere      } from "#/Benzaiten/shapes"
+import extract_surface from "@/Benzaiten"
 
 import Eunomia from "@/Eunomia"
 import Legion  from "@/Legion"
 
-import MainCamera from "@/Athenaeum/components/MainCamera"
-
-import Update_Camera_and_Target_Positions from "@/Athenaeum/systems/Update_Camera_and_Target_Positions"
-
-import Benzaiten from "@/Benzaiten"
-
-import SimpleVertexColor from "@/Athenaeum/materials/SimpleVertexColor"
-
 await Eunomia.init()
 
-const start = performance.now(),
-      mesh  = new Benzaiten(Sphere, 3).extract_surface(new Float32Array([/* radius */ .475])),
+const divisions  = 3,
+      segments   = new Float32Array([2, 2, 2]),
+      dimensions = segments[0] * segments[1] * segments[2],
+      voxels     = dimensions  * divisions,
+      vertices   = new Float32Array(voxels * dimensions * dimensions * divisions),
+      params     = new Float32Array([/* radius */ .475]),
+      shape      = Sphere(),
+      start = performance.now(),
+      mesh  = extract_surface(shape, divisions, segments, params, vertices),
       bench = (performance.now() - start).toFixed(1)
 
 console.log(`Mesh took ${bench} milliseconds to generate`)
