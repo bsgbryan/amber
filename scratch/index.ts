f2528ef6efa90f63ba357fddb41d411f2f0602f9
Bryan Maynard <bsgbryan@gmail.com>
2023-12-02 18:54:22 -0600

    Migrated extract_surface to WebAssembly ...
    *However* ... WebAssembly is processing the `if` checks for addign vertices differently than TS/JS did. I have no diea why. It looks like I'll need to rework that logic - which is quite frustrating.
    
    I did implement SIMD, though, which is cool; and it has had a pretty dramatic imact on perfomance! Mesh genration speed has increated by __3x__! 🤯🥳
    
    Now, to fix that damn boolean logic ...

--- scratch/index.ts
+++ scratch/index.ts
@@ -1,9 +1,11 @@
+import Color                              from "@/Athenaeum/Color"
 import MainCamera                         from "@/Athenaeum/components/MainCamera"
+import ColoredPoint                       from "@/Athenaeum/materials/ColoredPoint"
 import SimpleVertexColor                  from "@/Athenaeum/materials/SimpleVertexColor"
 import Update_Camera_and_Target_Positions from "@/Athenaeum/systems/Update_Camera_and_Target_Positions"
 
-import { Sphere      } from "#/Benzaiten/shapes"
-import extract_surface from "@/Benzaiten"
+import { Sphere  } from "#/Benzaiten/shapes"
+import { execute } from "#/Benzaiten/extract_surface"
 
 import Eunomia from "@/Eunomia"
 import Legion  from "@/Legion"
@@ -11,21 +13,47 @@ import Legion  from "@/Legion"
 await Eunomia.init()
 
 const divisions  = 3,
-      segments   = new Float32Array([2, 2, 2]),
+      segments   = new Uint8Array  ([2, 2, 2]),
+      space      = new Float32Array([1, 1, 1]),
+      origin     = new Float32Array([0, 0, 0]),
+      recursions = 1,
       dimensions = segments[0] * segments[1] * segments[2],
       voxels     = dimensions  * divisions,
       vertices   = new Float32Array(voxels * dimensions * dimensions * divisions),
       params     = new Float32Array([/* radius */ .475]),
       shape      = Sphere(),
-      start = performance.now(),
-      mesh  = extract_surface(shape, divisions, segments, params, vertices),
-      bench = (performance.now() - start).toFixed(1)
+      start   = performance.now(),
+      surface = execute(shape, params, vertices, divisions, segments, space, origin, recursions),
+      bench   = (performance.now() - start).toFixed(1)
 
 console.log(`Mesh took ${bench} milliseconds to generate`)
 
-new SimpleVertexColor().apply_to(mesh)
+new ColoredPoint(Color.from_html_rgb(255, 192, 64), .5).apply_to({
+  vertices: new Float32Array([-1, 0, 0]),
+})
+
+new ColoredPoint(Color.from_html_rgb(255, 64, 192), .5).apply_to({
+  vertices: new Float32Array([0, -1, 0]),
+})
+
+new ColoredPoint(Color.from_html_rgb(64, 192, 255), .5).apply_to({
+  vertices: new Float32Array([0, 0, -1])
+})
+
+new ColoredPoint(Color.from_html_rgb(192, 64, 255), .25).apply_to({
+  vertices: surface,
+})
+
+// TODO: Figure out why this line fails if the above ColoredPoints are not rendered first
+new SimpleVertexColor().apply_to({ vertices: surface })
 
 const camera = Legion.add_entity()
 
 Legion.add_system(new Update_Camera_and_Target_Positions())
 Legion.add_component(camera, new MainCamera(0, .15, -1.5))
+
+const extra   = vertices.length - surface.length,
+      ratio   = extra / vertices.length,
+      percent = (ratio * 100).toFixed(1)
+
+console.log(`${percent}% (${extra}) extra vertices`)
--- src/Benzaiten/types.ts
+++
@@ -1,6 +0,0 @@
-export type Vector3 = Float32Array
-
-export type Mesh = {
-  vertices: Float32Array
-  indices?: Uint16Array
-}
